78502001e0034624bab8af26e52318e179427435
pangwu86 <pangwu86@gmail.com>
2012-10-20 19:44:59 +0800

    tab变成空格

--- jqplug/jquery.password.js
+++ jqplug/jquery.password.js
@@ -18,61 +18,59 @@
  *      msg       : function(key){...}              # 获取多国语言字符串
  * }
  */
-(function($){
-	// 本函数将检验密码是否一致（如果二次密码不为空），以及指示出密码强度
-	function _do_check(selection, opt){
-		var jPwdNew = selection.find("input.pwd-new");
-		var jPwdRe = selection.find("input.pwd-re");
-		var pwd = jPwdNew.val();
-		var pwdRe = jPwdRe.val();
-		// 先检测密码强度
-		var m = !pwd || pwd.length<6 ? 0 : 
-						/[0-9]/.test(pwd)*1 + /[a-z]/.test(pwd)*1
-						+ /[A-Z]/.test(pwd)*1 + /[^0-9a-zA-Z]/.test(pwd)*1;
-		$(selection.find(".pwd-tester .pwd-test-cell").removeClass("pwd-test-on")[m])
-			.prevAll(".pwd-test-cell").andSelf()
-			.addClass("pwd-test-on");
+(function($) {
+    // 本函数将检验密码是否一致（如果二次密码不为空），以及指示出密码强度
+    function _do_check(selection, opt) {
+        var jPwdNew = selection.find("input.pwd-new");
+        var jPwdRe = selection.find("input.pwd-re");
+        var pwd = jPwdNew.val();
+        var pwdRe = jPwdRe.val();
+        // 先检测密码强度
+        var m = !pwd || pwd.length < 6 ? 0 : /[0-9]/.test(pwd) * 1 + /[a-z]/.test(pwd) * 1 + /[A-Z]/.test(pwd) * 1 + /[^0-9a-zA-Z]/.test(pwd) * 1;
+        $(selection.find(".pwd-tester .pwd-test-cell").removeClass("pwd-test-on")[m]).prevAll(".pwd-test-cell").andSelf().addClass("pwd-test-on");
 
-		// 如果两者一致 ...
-		if(pwdRe && pwd && pwdRe==pwd){
-			jPwdRe.removeClass("pwd-invalid");
-			selection.find(".pwd-verifier").empty();
-			if(typeof opt.on_ready == "function")
-				opt.on_ready.call(jPwdNew, pwd);
-		}
-		// 否则 ...
-		else if(pwdRe) {
-			jPwdRe.addClass("pwd-invalid");
-			selection.find(".pwd-verifier").text(opt.msg("pwd.unmatch"));
-			if(typeof opt.on_unmatch == "function")
-				opt.on_unmatch.call(selection, jPwdNew, jPwdRe);
-		}
-	}
-	// 扩展插件
-	$.fn.extend({
-		password : function(opt){
-			opt = opt || {msg : function(key){return key;}};
-			var selection = this;
+        // 如果两者一致 ...
+        if (pwdRe && pwd && pwdRe == pwd) {
+            jPwdRe.removeClass("pwd-invalid");
+            selection.find(".pwd-verifier").empty();
+            if (typeof opt.on_ready == "function") opt.on_ready.call(jPwdNew, pwd);
+        }
+        // 否则 ...
+        else if (pwdRe) {
+            jPwdRe.addClass("pwd-invalid");
+            selection.find(".pwd-verifier").text(opt.msg("pwd.unmatch"));
+            if (typeof opt.on_unmatch == "function") opt.on_unmatch.call(selection, jPwdNew, jPwdRe);
+        }
+    }
+    // 扩展插件
+    $.fn.extend({
+        password: function(opt) {
+            opt = opt || {
+                msg: function(key) {
+                    return key;
+                }
+            };
+            var selection = this;
 
-			// 首先绘制一个打分的框框
-			var html = '<table cellspacing="1" cellpadding="0"><tr>';
-			html += '<td class="pwd-title pwd-title-weak">' + opt.msg('pwd.weak') + '</td>';
-			for(var i=0; i<5; i++){
-				html += '<td class="pwd-test-cell">&nbsp;</td>';
-			}
-			html += '<td class="pwd-title pwd-title-strong">' + opt.msg('pwd.strong') + '</td>';
-			html += '</tr></table>';
-			selection.find(".pwd-tester").html(html);
+            // 首先绘制一个打分的框框
+            var html = '<table cellspacing="1" cellpadding="0"><tr>';
+            html += '<td class="pwd-title pwd-title-weak">' + opt.msg('pwd.weak') + '</td>';
+            for (var i = 0; i < 5; i++) {
+                html += '<td class="pwd-test-cell">&nbsp;</td>';
+            }
+            html += '<td class="pwd-title pwd-title-strong">' + opt.msg('pwd.strong') + '</td>';
+            html += '</tr></table>';
+            selection.find(".pwd-tester").html(html);
 
-			// 监视新密码输入动作
-			this.delegate("input.pwd-new", "keyup", function(){
-				_do_check(selection, opt);
-			});
-			// 监视二次密码输入动作
-			this.delegate("input.pwd-re", "keyup", function(){
-				_do_check(selection, opt);
-			});
-			return this;
-		}
-	});
+            // 监视新密码输入动作
+            this.delegate("input.pwd-new", "keyup", function() {
+                _do_check(selection, opt);
+            });
+            // 监视二次密码输入动作
+            this.delegate("input.pwd-re", "keyup", function() {
+                _do_check(selection, opt);
+            });
+            return this;
+        }
+    });
 })(window.jQuery);
