eb7a8962688ec38ea644e624797aae4fa752aa72
pangwu86 <pangwu86@gmail.com>
2012-10-23 06:37:28 +0800

    添加一个ui控件的模板，可以copy过去作为基础

---
+++ ui-js/ui.tmpl.js
@@ -0,0 +1,36 @@
+/**
+ *
+ */
+(function($, ui, $z) {
+    ui('', {
+        // 依次触发 on_init -> on_show -> on_resize -> on_listen -> on_ready
+        on_init: function() {
+            var html = '';
+            html += '';
+            this.selection.append(html);
+        },
+        on_show: function() {
+
+        },
+        on_resize: function(w, h) {
+
+        },
+        on_listen: function() {
+
+        },
+        on_ready: function() {
+
+        },
+        on_depose: function() {
+
+        },
+        // 绑定事件
+        events: {
+
+        },
+        // 自定义方法，通常作为与其他ui交互的接口
+        methods: {
+
+        }
+    });
+})(window.jQuery, window.NutzUI, window.NutzUtil);
